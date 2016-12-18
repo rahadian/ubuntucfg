c9c1880002037ef3a402a0c4c4a5e42fa2aef68f
AryaTux <rahadian271094@gmail.com>
2016-12-18 12:55:21 +0700

    Update snort2.sh

--- snort2.sh
+++ snort2.sh
@@ -1,4 +1,1 @@
-wget http://tux101010.vv.si/barnyard2-master.zip
-wget http://tux101010.vv.si/daq-2.0.6.tar.gz
-wget http://tux101010.vv.si/daq-2.2.1.tar.gz
 apt-get install -y mysql-server libmysqlclient-dev mysql-client autoconf libtool
