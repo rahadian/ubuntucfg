36dc0e80df584bd80c99c2f575c98885a472d71d
AryaTux <rahadian271094@gmail.com>
2016-12-18 13:08:56 +0700

    Add files via upload

---
+++ all.sh
@@ -0,0 +1,14 @@
+apt-get install apache2 phpmyadmin mysql openssl ssl-cert wireshark
+apt-get install iptables-persistent vsftpd openssh-server
+apt-get install build-essential
+apt-get install libpcap-dev libpcre3-dev libdumbnet-dev
+apt-get install snort
+apt-get install bison flex
+apt-get install -y mysql-server libmysqlclient-dev mysql-client autoconf libtool
+apt-get install postfix postfix-mysql postfix-doc mysql-client mysql-server courier-authdaemon courier-authlib-mysql courier-pop courier-pop-ssl courier-imap courier-imap-ssl libsasl2-2 libsasl2-modules libsasl2-modules-sql sasl2-bin libpam-mysql openssl phpmyadmin apache2 libapache2-mod-php5 php5 php5-mysql libpam-smbpass
+apt-get install squirrelmail
+apt-get install build-essential make libssl-dev git
+apt-get install mysql-server libmysqlclient-dev mysql-client apache2 php5 libapache2-mod-php5 php5-mysql php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl
+apt-get install openvpn easy-rsa
+apt-get install ssh
+
